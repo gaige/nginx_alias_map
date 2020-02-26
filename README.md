8a2ae4f0edb3c8c7eaff81ae4add93f4cf4b405a
Gaige B. Paulsen <gaige@cluetrust.com>
2020-02-26 10:56:05 -0500

    add support for querystrings

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ Add the directory to the base plugins directory to `PLUGIN_PATHS` in
 
 Definable parameters (with defaults in brackets) allow some configuration of the output
 of the plugin.
+
 There are two definable parameters, one from Chris's code (`ALIAS\_DELIMITER`), which
 defines the delimiter for multiple aliases for the same item; and `ALIAS\_FILE`, which
 defines the final name of the output file containing the map; and 
@@ -23,29 +24,38 @@ defines the final name of the output file containing the map; and
     ALIAS_DELIMITER : Delimeter between multiple aliases for the same item [","]
     ALIAS_FILE : Name of map file to be placed in `output` ['alias_map.txt']
     ALIAS_MAP : Name of the map used in the alias file ['redirect_uri']
+    ALIAS_MAP_TEMP: Name of the map used in the alias file when 2-stage lookup is needed ['redirect_uri_1']
+
+### Support for URLs with query strings
+
+In the event that you need to redirect a URI that contains a query string, a separate
+map block will be created to map the `$request_uri` against an re.escaped version of your
+alias that contains the `?` character. Otherwise, when no query string is present, the
+test is made against `$uri`, which has much more processing done with it (query string
+removal, removal of unnecessary '/'s, and so forth).
 
 ### NGINX configuration
 
 The resulting file (stored in `output/$(ALIAS\_FILE)`) is ready to be included into
-your nginx configuration file.  Once the map is created, use the `ALIAS_MAP` variable
-in your processing.
+your nginx configuration file (in an http stanza). Once the map is created, use the
+`ALIAS_MAP` variable in your processing.
 
-	include /opt/web/output/alias_map.txt;
-	
-	server {
-	  listen       *:80 ssl;
-	  server_name  example.server;
+    include /opt/web/output/alias_map.txt;
+    
+    server {
+      listen       *:80 ssl;
+      server_name  example.server;
 
 
-		# Redirection logic
-		if ( $redirect_uri ) {
-			return 301 $redirect_uri;
-		}
+        # Redirection logic
+        if ( $redirect_uri ) {
+            return 301 $redirect_uri;
+        }
 
-		location / {
-			alias /opt/web/output;
-		}
-	}
+        location / {
+            alias /opt/web/output;
+        }
+    }
 
 This configuration uses the evil `if` statement, but it's concise.  If you have a better
 approach, please create a pull request and I'll add it to this doc (or replace it if it
@@ -53,3 +63,4 @@ makes more sense).
 
 I've chosen to use a 301 redirect here, because I'm confident of the permanency.  During
 testing, you may want to use a 302.
+
--- nginx_alias_map.py
+++ nginx_alias_map.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 import os.path
 import logging
+from re import escape
 from urllib.parse import urlparse
 
 from pelican import signals
@@ -18,32 +19,60 @@ class NginxAliasMapGenerator(object):
         self.alias_delimiter = settings.get('ALIAS_DELIMITER', ',')
         self.alias_file = settings.get('ALIAS_FILE','alias_map.txt')
         self.alias_map = settings.get('ALIAS_MAP', 'redirect_uri')
+        self.alias_map_temp = settings.get('ALIAS_MAP_TEMP', self.alias_map+'_1')
 
     def create_alias(self, page, alias, fd):
         partial_url = page.url
         if not urlparse(partial_url).scheme:
             partial_url = "https://$server_name/"+partial_url
-        alias_line = "\t~^%s$ %s;" % (alias, partial_url)       
+        quoted_alias = escape(alias)
+        alias_line = "\t~^%s$ %s;" % (quoted_alias, partial_url)       
         fd.write(alias_line+"\n")
 
     def generate_output(self, writer):
         path = os.path.join(self.output_path, self.alias_file)
+
+        pages = (
+            self.context['pages'] + self.context['articles'] +
+            self.context.get('hidden_pages', []))
+
+        query_aliases = []
+        noquery_aliases = []
+        for page in pages:
+            aliases = page.metadata.get('alias', [])
+            if type(aliases) != list:
+                aliases = aliases.split(self.alias_delimiter)
+            for alias in aliases:
+                alias = alias.strip()
+                if '?' in alias:
+                    query_aliases += [(page,alias)]
+                    logger.info('[alias] Saving query alias %s' % alias)
+                else:                   
+                    noquery_aliases += [(page,alias)]
+                    logger.info('[alias] Saving alias %s' % alias)
+
         with open(path, 'w') as fd:
-            fd.write("map $uri $%s {\n" % self.alias_map)
+            default_variable = None
+            if len(noquery_aliases) > 0:
+                if len(query_aliases) > 0:
+                    default_variable = self.alias_map_temp
+                    fd.write("map $uri $%s {\n" % default_variable)
+                else:
+                    fd.write("map $uri $%s {\n" % self.alias_map)
 
-            pages = (
-                self.context['pages'] + self.context['articles'] +
-                self.context.get('hidden_pages', []))
-
-            for page in pages:
-                aliases = page.metadata.get('alias', [])
-                if type(aliases) != list:
-                    aliases = aliases.split(self.alias_delimiter)
-                for alias in aliases:
-                    alias = alias.strip()
-                    logger.info('[alias] Processing alias %s' % alias)
+                for (page,alias) in noquery_aliases:
+                    logger.info('[alias] Processing quoted alias %s' % alias)
                     self.create_alias(page, alias, fd)
-            fd.write("  }\n")
+                fd.write("  }\n")
+
+            if len(query_aliases) > 0:
+                fd.write("\nmap $request_uri $%s {\n" % self.alias_map)
+                if default_variable:
+                    fd.write("\tdefault $%s;\n" % default_variable)
+                for (page,alias) in query_aliases:
+                    logger.info('[alias] Processing quoted alias %s' % alias)
+                    self.create_alias(page, alias, fd)
+                fd.write("  }\n")
 
 
 def get_generators(generators):
